449deeeee497d60d0ddc65b682a80b34768d86b3
Yazan <kalitest928ya@gmail.com>
2026-07-18 21:32:49 -0700

    deleted the mock data

--- iblockchain/scripts/fix-storage-rls.sql
+++
@@ -1,5 +0,0 @@
-DROP POLICY IF EXISTS "Admins can view all KYC files" ON storage.objects;
-CREATE POLICY "Admins can view all KYC files" ON storage.objects FOR SELECT
-  USING (bucket_id = 'kyc-documents' AND public.is_admin());
-
--- Update the main migration file contents are already correct
